e6541961c137d0058eca4e05f9def04a5a62910f
Mahesh <kethamaheshwar@gmail.com>
2022-04-18 11:57:25 +0530

    modified

--- JS/keyboardPress.js
+++ JS/keyboardPress.js
@@ -6,9 +6,15 @@ const puppeteer = require('puppeteer');
     await page.waitForSelector('.a4bIc');
     await page.type('.a4bIc','dragon ball k');
     await page.waitForTimeout('2000');
-    await page.keyboard.press('Backspace',{delay:10});
+    await page.keyboard.press('Backspace',{delay:10});//work with keyboard
     await page.type('.a4bIc','z');
-    await page.keyboard.press('Enter',{delay:10});
-    await page.waitForTimeout('2000');
-    await browser.close();
+    await page.keyboard.press('Enter',{delay:10});//work with keyboard
+    await page.waitForXPath("//a[text()='Images']");//work with xpath
+   // const [images] = await page.$x("//a[text()='Images']");//in-order to use xpath we need to create an object
+   // await images.click();
+   await page.$x("//a[text()='Images']").then(async Image =>{
+       await Image[0].click();                              //modified way to use xpath without creating an object
+   } )
+    await page.waitForTimeout('3000');
+    await browser.close();  
 })();
