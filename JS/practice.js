6cf20811b06684dcd46f01e967ac8dc4007aa326
Mahesh <kethamaheshwar@gmail.com>
2022-04-20 18:46:56 +0530

    add to cart code push

--- JS/practice.js
+++ JS/practice.js
@@ -7,6 +7,19 @@ const puppeteer = require('puppeteer');
     await page.type('#twotabsearchtextbox','amazon eco dot');
     await page.waitForTimeout('3000');
     await page.keyboard.press('Enter',{delay:10});
-    await page.waitForTimeout('3000');
-   // await browser.close();
+    await page.waitForXPath("//span[contains(text(),'Echo Dot (3rd Gen) - #1 smart speaker brand in Ind')]");
+    //await page.$x("//span[contains(text(),'Echo Dot (3rd Gen) - #1 smart speaker brand in Ind')]").then(async picture =>{
+     //   await picture[0].click();
+  //  }) 
+  await page.waitForTimeout('3000');
+    const [picture] = await page.$x("//span[contains(text(),'Echo Dot (3rd Gen) - #1 smart speaker brand in Ind')]");
+
+    await picture.click();
+    await page.waitForSelector('#quantity');
+    await page.select('#quantity','2');
+   await page.waitForSelector('#add-to-cart-button');
+  await page.click('#add-to-cart-button',{clickCount: 1});
+  
+    await page.waitForTimeout('5000');
+    await browser.close();
 })();
